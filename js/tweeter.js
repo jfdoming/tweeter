bca0e37a5f6e1e9d424aa78bedb9e6078ace0518
Julian Frank Dominguez-Schatz <dolphinspray.fr@gmail.com>
2019-04-16 17:12:29 -0400

    Added more features.

--- js/tweeter.js
+++ js/tweeter.js
@@ -6,6 +6,82 @@
 		return !SCHEDULE_INSTANT * -SCHEDULE_MEAN_TIME * Math.log(Math.random());
 	};
 	
+	
+	const TIMER_INTERVAL = 50;
+	
+	let visibleTweets = [];
+	setInterval(() => {
+		const now = Date.now();
+		
+		const manage = (t, lcsname, lcpname, ucpname) => {
+			const maxk = "max" + ucpname;
+			const valk = lcpname;
+			const timek = lcsname + "Time";
+			const sizek = lcsname + "Size";
+			const freqk = lcsname + "Frequency";
+			const elk = lcsname + "El";
+			if ((t[maxk] == -1 || t[valk] < t[maxk]) && now - t[timek] > (1000 / t[freqk])) {
+				t[valk] += t[sizek];
+				if (t[maxk] != -1 && t[valk] > t[maxk]) {
+					t[valk] = t[maxk];
+				}
+				
+				t[timek] = now;
+				t[elk].textContent = t[valk];
+			}
+		};
+		
+		visibleTweets.forEach((t) => {
+			if (now - t.eTime > 1000) {
+				++t.time;
+				t.eTime = now;
+				
+				if (t.time < 24) {
+					t.timeEl.textContent = t.time + "h";
+				} else if (t.time < 24 * 30) {
+					t.timeEl.textContent = Math.floor(t.time / 24) + "d";
+				} else if (t.time < 24 * 365) {
+					t.timeEl.textContent = Math.floor(t.time / (24 * 30)) + "mo";
+				} else {
+					t.timeEl.textContent = Math.floor(t.time / (24 * 365)) + "y";
+				}
+			}
+			
+			manage(t, "comment", "comments", "Comments");
+			manage(t, "retweet", "retweets", "Retweets");
+			manage(t, "like", "likes", "Likes");
+			/*if ((t.maxComments == -1 || t.comments < t.maxComments) && now - t.commentTime > (1000 / t.commentFrequency)) {
+				t.comments += t.commentSize;
+				if (t.maxComments != -1 && t.comments > t.maxComments) {
+					t.comments = t.maxComments;
+				}
+				
+				t.commentTime = now;
+				t.commentEl.textContent = t.comments;
+			}
+			
+			if ((t.maxRetweets == -1 || t.retweets < t.maxRetweets) && now - t.retweetTime > (1000 / t.retweetFrequency)) {
+				t.retweets += t.retweetSize;
+				if (t.maxRetweets != -1 && t.retweets > t.maxRetweets) {
+					t.retweets = t.maxRetweets;
+				}
+				
+				t.retweetTime = now;
+				t.retweetEl.textContent = t.retweets;
+			}
+			
+			if ((t.maxLikes == -1 || t.likes < t.maxLikes) && now - t.likeTime > (1000 / t.likeFrequency)) {
+				t.likes += t.likeSize;
+				if (t.maxLikes != -1 && t.likes > t.maxLikes) {
+					t.likes = t.maxLikes;
+				}
+				
+				t.likeTime = now;
+				t.likeEl.textContent = t.likes;
+			}*/
+		});
+	}, TIMER_INTERVAL);
+	
 	let el = (type, className, parent = null) => {
 		let e = document.createElement(type);
 		
@@ -60,6 +136,18 @@
 	let scheduled = false;
 	let tweetSchedule = [];
 	
+	document.addEventListener("keypress", (e) => {
+		if (e.code == "Space") {
+			if (tweetSchedule.length <= 0) {
+				return;
+			}
+			
+			e.preventDefault();
+			let scheduledTweet = tweetSchedule.shift();
+			tweeter.tweet(...scheduledTweet);
+		}
+	});
+	
 	window.tweeter = {
 		users: {
 			push: (handle, fullname = "--", verified = false, profileExtension = ".png") => {
@@ -104,13 +192,39 @@
 			},
 		},
 		
-		tweet: (handle, tweetBodyText = "...", time = "2h", embedUrl = "") => {
+		tweet: (handle, tweetBodyText = "...", embedUrl = "", timerParams = {}, replyUsers = []) => {
 			let user = tweeter.users.find(handle);
 			
 			let fullpage = document.getElementById("fullpage");
 			if (!fullpage) {
 				return;
 			}
+			
+			const randScale = (rangeSize = 0.25) => {
+				return (1 - rangeSize) + (2 * Math.random() * rangeSize);
+			};
+			
+			const now = Date.now();
+			let tweetObj = {};
+			tweetObj.eTime = now;
+			tweetObj.commentTime = now;
+			tweetObj.retweetTime = now;
+			tweetObj.likeTime = now;
+			tweetObj.commentFrequency = ((timerParams["commentFrequency"] || 0.5) * randScale(0.1));
+			tweetObj.retweetFrequency = ((timerParams["retweetFrequency"] || 0.1) * randScale(0.1));
+			tweetObj.likeFrequency = ((timerParams["likeFrequency"] || 0.9) * randScale(0.1));
+			tweetObj.commentSize = timerParams["commentSize"] || 1;
+			tweetObj.retweetSize = timerParams["retweetSize"] || 1;
+			tweetObj.likeSize = timerParams["likeSize"] || 1;
+			tweetObj.maxComments = Math.ceil(timerParams["maxComments"] * randScale()) || -1;
+			tweetObj.maxRetweets = Math.ceil(timerParams["maxRetweets"] * randScale()) || -1;
+			tweetObj.maxLikes = Math.ceil(timerParams["maxLikes"] * randScale()) || -1;
+			tweetObj.time = 0;
+			tweetObj.comments = 0;
+			tweetObj.retweets = 0;
+			tweetObj.likes = 0;
+			console.log(tweetObj);
+			console.log(timerParams);
 			
 			let section = div("section");
 			fullpage.insertBefore(section, fullpage.firstChild);
@@ -128,12 +242,20 @@
 			
 			span("username", accGroup, "@" + handle);
 			span("middot", accGroup, "\u00B7");
-			span("time", accGroup, time);
+			tweetObj.timeEl = span("time", accGroup, "Just now");
 			
 			let tweetBody = div("tweet-body", body);
+			if (replyUsers.length > 0) {
+				let text = "Replying to";
+				replyUsers.forEach((u) => {
+					text += " <a>@" + u + "</a>";
+				});
+				let replyText = span("reply-text", tweetBody);
+				replyText.innerHTML = text;
+			}
 			let tweetText = div("tweet-text", tweetBody);
 			
-			const URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+			const URL_REGEX = /((http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*))/g;
 			tweetText.innerHTML = tweetBodyText.replace(/((?:#|@)\w+)(\b)/g, "<a>$1</a>$2").replace(URL_REGEX, "<a>$1</a>");
 			
 			if (tweeter.embeds.contains(embedUrl)) {
@@ -148,9 +270,9 @@
 			}
 			
 			let footer = div("tweet-footer", body);
-			action(footer, "message", "14");
-			action(footer, "retweet", "14");
-			action(footer, "heart", "14");
+			tweetObj.commentEl = action(footer, "message", "0").querySelector(".tweet-footer-label");
+			tweetObj.retweetEl = action(footer, "retweet", "0").querySelector(".tweet-footer-label");
+			tweetObj.likeEl = action(footer, "heart", "0").querySelector(".tweet-footer-label");
 			action(footer, "dm");
 			
 			fullpage.classList.remove("entered");
@@ -161,6 +283,8 @@
 				fullpage.style.transform = "";
 				fullpage.classList.add("entered");
 			}, 0);
+			
+			visibleTweets.push(tweetObj);
 		},
 		schedule: (...tweetToSchedule) => {
 			if (!scheduled) {
@@ -168,6 +292,7 @@
 					let time = randomTime();
 					setTimeout(() => {
 						if (tweetSchedule.length <= 0) {
+							scheduled = false;
 							return;
 						}
 						
--- js/tweets.js
+++ js/tweets.js
@@ -3,7 +3,38 @@
 		return;
 	}
 	
+	const NORMAL = {
+		commentFrequency: 0.5,
+		retweetFrequency: 0.1,
+		likeFrequency: 0.9,
+		commentSize: 1,
+		retweetSize: 1,
+		likeSize: 1,
+		maxComments: 10,
+		maxRetweets: 4,
+		maxLikes: 50
+	};
+	
+	const CONTROVERSIAL = {
+		commentFrequency: 2,
+		retweetFrequency: 0.8,
+		likeFrequency: 0.3,
+		commentSize: 3,
+		retweetSize: 1,
+		likeSize: 1,
+		maxComments: 569,
+		maxRetweets: 87,
+		maxLikes: 300
+	};
+	//		tweetObj.commentSize = timerParams["commentSize"] || 1;
+	//		tweetObj.retweetSize = timerParams["retweetSize"] || 1;
+	//		tweetObj.likeSize = timerParams["likeSize"] || 1;
+	//		tweetObj.maxComments = timerParams["maxComments"] || -1;
+	//		tweetObj.maxRetweets = timerParams["maxRetweets"] || -1;
+	//		tweetObj.maxLikes = timerParams["maxLikes"] || -1;
+	
 	// Load the scheduled tweets.
-	tweeter.schedule("Cmdr_Hadfield", "Thanks, Vancouver, for the beautiful evening.", "2h", "renewables");
-	tweeter.schedule("SchatzEd", "Wow, my son @jfdoming just made this really cool app! Check it out here! http://example.com/ #cool", "5m");
+	tweeter.schedule("Cmdr_Hadfield", "Thanks, Vancouver, for the beautiful evening.", "renewables", CONTROVERSIAL);
+	tweeter.schedule("SchatzEd", "Wow, my son @jfdoming just made this really cool app! Check it out here! example.com #cool");
+	tweeter.schedule("Cmdr_Hadfield", "Wow, @SchatzEd, that's really #cool!", "renewables", NORMAL, ["SchatzEd"]);
 })(window.tweeter);
